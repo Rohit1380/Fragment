dd5f80567264a3e01b870ec6dbd6f8314ce885be
priyanshukumar2112 <>
2023-03-03 23:43:35 -0800

    Initial commit

---
+++ app/src/main/java/com/rohit/fragment/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.rohit.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+//    override fun onStart() {
+//        super.onStart()
+//        Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show()
+//    }
+//
+//
+//    override fun onPause() {
+//        super.onPause()
+//        Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show()
+//
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        Toast.makeText(this, "Resume", Toast.LENGTH_SHORT).show()
+//
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show()
+//
+//    }
+//
+//    override fun onRestart() {
+//        super.onRestart()
+//        Toast.makeText(this, "Restart", Toast.LENGTH_SHORT).show()
+//
+//    }
+}
